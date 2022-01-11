a406f3e146fa707247543fd64f1714470edb5e8c
athulya-k <77480517+Athulya-k@users.noreply.github.com>
2022-01-11 12:22:57 +0530

    home page of admin created

---
+++ a_home.php
@@ -0,0 +1,42 @@
+<?php
+	session_start();
+	$fid=$_GET["fid"];
+	if($_SESSION["username"] == NULL)
+	{
+		header("Location: ../../index.php");
+	}
+	else
+	{
+?>
+<html>
+	<head>
+		<title>Online Voting System</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="../../css/style2.css">
+		<link rel="icon" href="../../img/icon.jpg">
+		<style>
+			body,html{
+				background:url(../../img/banner-bg.jpg) no-repeat;
+				background-size:cover;
+			}
+		</style>
+	</head>
+	<body>
+		<ul>
+			<li><a href="a_home.php">HOME</a></li>
+			<li style="float:right;"><a href="a_logout.php">LOGOUT</a></li>
+		</ul>
+		<br>
+		<table align="right" cellspacing="20">
+			<tr><th><a href="a_ballot_list.php"><input type="button" class="button4" value="VIEW BALLOT LIST"></a></th></tr>
+			<tr><th><a href="a_voters_list.php"><input type="button" class="button4" value="VIEW VOTER LIST"></a></th></tr>
+			<tr><th><a href="a_candidates_list.php"><input type="button" class="button4" value="VIEW CANDIDATE LIST"></a></th></tr>
+			<tr><th><a href="a_voter_approval.php"><input type="button" class="button4" value="VOTERS APPROVAL"></a></th></tr>
+			<tr><th><a href="../candidate/c_register.php"><input type="button" class="button4" value="REGISTER NEW CANDIDATE"></a></th></tr>
+			<tr><th><a href="../voter/v_fingerid.php"><input type="button" class="button4" value="REGISTER NEW VOTER"></a></th></tr>
+		</table>
+	</body>
+</html>
+<?php
+	}
+?>
