98de92f00ba25d574a55422800b27e5d2fcceabf
athulya-k <77480517+Athulya-k@users.noreply.github.com>
2022-01-11 12:13:44 +0530

    index.php added

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+?>
+<html>
+	<head>
+		<title>Online Voting System</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="css/style.css">
+		<link rel="icon" href="img/icon.jpg">
+	</head>
+	<body>
+		<ul>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="php/voter/v_login.php">Voter</a></li>
+			<li><a href="php/admin/a_results.php">Election Status & Results</a></li>
+			<li><a href="php/search.php">Electoral Roll Search</a></li>
+			<li><a href="admin/admin.php">Admin</a></li>
+		</ul>
+	</body>
+</html>
+
