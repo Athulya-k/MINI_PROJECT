2b4af911ac2acbe449bcd24721c9d05812122088
chithrapa <85601140+chithrapa@users.noreply.github.com>
2022-02-11 20:18:47 +0530

    v_ register added

---
+++ v_register.php
@@ -0,0 +1,167 @@
+<?php
+include("../dbconnect.php");
+session_start();
+?>
+<html>
+<head>
+<title>Online Voting System</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="../../css/style1.css">
+<link rel="icon" href="../../img/icon.jpg">
+<script>
+
+function validate()
+{
+var name1= /^[a-z A-Z]+$/; //pattern allowed alphabet a-z or A-Z
+//var party1= /^[a-z A-Z]+$/; //pattern allowed alphabet a-z or A-Z
+//var ph= /^[0-9]{10}$/;
+                        //var email_valid= /^[\w\d\.]+\@[a-zA-Z\.]+\.[A-Za-z]{1,4}$/; //pattern valid email validation
+//var password_valid=/^[A-Z a-z 0-9 !@#$%&*()<>]{6,12}$/; //pattern password allowed A to Z, a to z, 0-9, !@#$%&*()<> character
+
+var name = document.getElementById("name"); //textbox id fname
+           // var party = document.getElementById("party"); //textbox id lname
+            //var email = document.getElementById("email"); //textbox id email
+            //var phone= document.getElementById("phone");
+            //var password = document.getElementById("password"); //textbox id password
+            var age= document.getElementById("age");
+
+if(!name1.test(name.value) || name.value=='')
+            {
+alert("Enter Name Alphabets Only....!");
+                name.focus();
+                name.style.background = '#f08080';
+                return false;                    
+            }
+
+           
+
+             
+            if(age.value<18||age.value>150|| age.value=='')
+            {
+//alert("Cannot register!Age invalid....!");
+                age.focus();
+                age.style.background = '#f08080';
+                return false;                    
+            }
+
+
+
+}
+
+               
+function setAge()
+{
+var dob = document.getElementById("dob"); //textbox id fname
+                         
+                        var today=new Date();
+                        var dob1=new Date(dob.value);
+                        var d=Math.abs(dob1.getTime()-today.getTime());
+                         var age1=Math.floor(d/(1000*60*60*24*365));
+                         (document.getElementById("age")).value=age1;
+                       
+}
+
+</script>
+</head>
+<body>
+<ul>
+<li><a href="../staff/s_home.php">HOME</a></li>
+<li style="float:right;"><a href="../staff/a_logout.php">LOGOUT</a></li>
+</ul>
+<form action="" method="POST" class="box" onSubmit="return validate();" enctype="multipart/form-data">
+<h1>VOTER REGISTRATION</h1>
+<table align="center">
+<tr>
+<th>NAME</th>
+<td><input type="text" id="name" name="username" class="inp" required></td>
+</tr>
+
+<tr>
+<th>DATE OF BIRTH:</th>
+<td><input type="date" id="dob" name="dob" class="inp" onChange="setAge();" required> </td>
+</tr>
+<tr>
+<th>AGE:</th>
+<td><input type="text" name="age" id="age" class="inp" readonly required></td>
+</tr>
+<tr>
+<th>GENDER:</th>
+<td><input type="radio" name="gender" value="Male" >Male&nbsp;
+<input type="radio" name="gender" value="Female">Female&nbsp;
+<input type="radio" name="gender" value="Others">Others</td>
+</tr>
+
+<tr>
+<th>DEPARTMENT:</th>
+<td><input type="department" id="department" name="department" class="inp" required></td>
+</tr>
+<tr>
+<th>ADMISSION NO:</th>
+<td><input type="admission" id="admission" name="admission" class="inp" required></td>
+</tr>
+<tr>
+<th>FINGERPRINT</th>
+<td><input type="file" name="file" id="file" class="inp"></td>
+</tr>
+
+ <tr>
+<td><input type="reset" value="reset" name="r" class="reset"></td>
+<td><input type="submit" value="submit" name="submit"class="submit"></td>
+</tr>
+</table>
+</form>
+
+<?php
+if(isset($_POST['submit']))
+{
+$uname=$_POST['username'];
+
+$dob=$_POST['dob'];
+$age=$_POST['age'];
+$gender=$_POST['gender'];
+
+
+$dept=$_POST['department'];
+$adno=$_POST['admission'];
+
+
+//***********************IMAGE DETAILS*************************
+$fileName = $_FILES['file']['name'];
+$fileSize = $_FILES['file']['size'];
+$fileTmpName  = $_FILES['file']['tmp_name'];
+$fileType = $_FILES['file']['type'];
+//***********************IMAGE EXTENTION***********************
+$fileExtension = strtolower(end(explode('.',$fileName)));
+//***********************NEW IMAGE NAME************************
+$photo=$uname.".".$fileExtension;
+
+
+$uid=substr(uniqid(),6);
+$password=substr(uniqid(),6);
+
+
+$sql="INSERT INTO tbl_voter
+values(id,'".$uid."','".$uname."','".$dob."','".$age."','".$gender."','".$dept."','".$adno."','".$photo."','".$password."',CURRENT_TIMESTAMP())";
+if ($conn->query($sql) === TRUE)
+{
+if(move_uploaded_file($fileTmpName, "../uploads/".$photo))
+{
+
+
+
+$_SESSION["cname"] = $uname;
+echo"<script>alert('Registration is done Successfully.Your UserID is : $uid and Your Password is : $password');
+window.location.replace('../staff/s_home.php');</script>";
+
+}
+}
+else
+{
+echo "Error: " . $sql . "<br>" . $conn->error;
+}
+$conn->close();
+}
+?>
+</body>
+</html>
+
